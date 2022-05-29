5aeb26b200c6120f2189261595f4c85ff9ed48af
Simen August Tinderholt <simtind@users.noreply.github.com>
2022-05-29 02:41:32 +0200

    Builds both app and net fw online

--- boards/arm/blaatind/board.cmake
+++ boards/arm/blaatind/board.cmake
@@ -1,8 +1,15 @@
 # Copyright (c) 2022 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=nrf53" "--speed=4000")
-board_runner_args(pyocd "--target=nrf5340_cpuapp" "--frequency=4000000")
+
+if(CONFIG_BOARD_BLAATIND_CPUAPP)
+    board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+    board_runner_args(pyocd "--target=nrf5340_cpuapp" "--frequency=4000000")
+elseif(CONFIG_BOARD_BLAATIND_CPUAPP)
+    board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
+    board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
+endif()
+
 
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
